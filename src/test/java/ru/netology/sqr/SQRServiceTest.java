8a369a8fda838750be3629a74157614113c921f1
Regina Timirgaleeva <sadikovaregina@mail.ru>
2023-06-17 21:07:01 +0300

    new file bonus.csv

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -3,16 +3,18 @@ package ru.netology.sqr;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class SQRServiceTest {
 
     @ParameterizedTest
-    @CsvSource({
-            "200,300,3",
-            "200,200,0",
-            "400,200,0"
-    })
+//    @CsvSource({
+//    "200,300,3",
+//    "200,200,0",
+//    "400,200,0"
+//    })
+    @CsvFileSource(files = "src/test/resources/bonus.csv")
     public void testWhenFewSquaresFound(int min, int max, int expected) {
         SQRService service = new SQRService();
 
