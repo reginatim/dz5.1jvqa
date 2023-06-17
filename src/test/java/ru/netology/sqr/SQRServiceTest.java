30fcb82a4111e2c1c4cb77b50a9050ad0c3c1d6f
Regina Timirgaleeva <sadikovaregina@mail.ru>
2023-06-17 20:44:18 +0300

    parameterized test

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -2,36 +2,41 @@ package ru.netology.sqr;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class SQRServiceTest {
 
-    @Test
-    public void testWhenFewSquaresFound() {
+    @ParameterizedTest
+    @CsvSource({
+            "200,300,3",
+            "200,200,0",
+            "400,200,0"
+    })
+    public void testWhenFewSquaresFound(int min, int max, int expected) {
         SQRService service = new SQRService();
 
-        int actual = service.calc(200, 300);
-        int expected = 3;
+        int actual = service.calc(min, max);
+        // int expected = 3;
 
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    public void testWhenValuesSame() {
-        SQRService service = new SQRService();
+//    @Test
+//public void testWhenValuesSame() {
+//SQRService service = new SQRService();
+//int actual = service.calc(200, 200);
+// int expected = 0;
+// Assertions.assertEquals(expected, actual);
+    //  }
 
-        int actual = service.calc(200, 200);
-        int expected = 0;
+//    @Test
+//    public void testWhenOneValueInsideAnother() {
+//        SQRService service = new SQRService();
 
-        Assertions.assertEquals(expected, actual);
-    }
+//        int actual = service.calc(400, 200);
+//        int expected = 0;
 
-    @Test
-    public void testWhenOneValueInsideAnother() {
-        SQRService service = new SQRService();
-
-        int actual = service.calc(400, 200);
-        int expected = 0;
-
-        Assertions.assertEquals(expected, actual);
-    }
+//        Assertions.assertEquals(expected, actual);
+//    }
 }
